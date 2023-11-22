b89e2d5acaab392230b485dda5e5585f8bd56e0b
Sergey-Eskhevits <sekshevits@gmail.com>
2023-11-22 13:50:25 +0400

    add README

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -29,13 +29,13 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT)
-
+#
 # from rest_framework import permissions
 # from drf_yasg.views import get_schema_view
 # from drf_yasg import openapi
-#
-# ...
-#
+# #
+# # ...
+# #
 # schema_view = get_schema_view(
 #     openapi.Info(
 #         title="Cats API",
