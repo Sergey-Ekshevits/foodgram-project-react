65b4484e10533dbecb65cc926647e0cae856c1d4
Sergey-Eskhevits <sekshevits@gmail.com>
2023-11-30 00:08:17 +0400

    statics

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -25,9 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'Not-empty-line')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', '')
-
-ALLOWED_HOSTS = ['foodgramprojects.sytes.net', '158.160.68.174', 'localhost']
+# DEBUG = os.getenv('DEBUG', '')
+DEBUG = True
+#ALLOWED_HOSTS = ['foodgramprojects.sytes.net', '158.160.68.174', 'localhost']
+ALLOWED_HOSTS = []
 
 # Application definition
 
